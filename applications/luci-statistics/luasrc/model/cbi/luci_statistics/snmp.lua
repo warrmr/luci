59881a6e5a3f6834e5f1e1513fe53351f8a5d14b
Matt Warrillow <mattwarrillow@warrillow.co.uk>
2013-03-17 15:42:01 +0000

    Updated configuration section, Added help tooltips and fixed issue that forced the user to add all values in quotes

--- applications/luci-statistics/luasrc/model/cbi/luci_statistics/snmp.lua
+++ applications/luci-statistics/luasrc/model/cbi/luci_statistics/snmp.lua
@@ -14,7 +14,7 @@ You may obtain a copy of the License at
 
 m = Map("luci_statistics",
 	translate("SNMP Plugin Configuration"),
-	translate("The SNMP plugin collects information from network devices."))
+	translate('The SNMP plugin collects information from network devices, <a href="http://collectd.org/documentation/manpages/collectd-snmp.5.shtml">collectd-snmp(5).</a>'))
 
 -- collectd_snmp config section
 s = m:section(NamedSection, "collectd_snmp", "luci_statistics" )
@@ -26,41 +26,47 @@ enable.default = 0
 -- collectd_snmp_data config section
 snmp = m:section(TypedSection, "collectd_snmp_data",
 	translate("SNMP Data Section"),
-	translate("Here you define the datatypes you wish to collect from your hosts" ))
+	translate("The Data block defines a list of values or a table of values that are to be queried." ))
 	snmp.addremove = true
 	snmp.anonymous = true
 	
 -- <Data "std_traffic">
-snmp_data_dataname = snmp:option(Value, "DataName", translate("Data Type Name"))
+snmp_data_dataname = snmp:option(Value, "DataName", translate("Data Block Name"))
 snmp_data_dataname.default = "std_traffic"
 
 -- collectd_snmp_data.type
-snmp_data_type = snmp:option(Value, "Type", translate("Type"))
+snmp_data_type = snmp:option(Value, "Type", translate("Type"), translate("collectd's type that is to be used, e. g. 'if_octets' for interface traffic or 'users' for a user count. The types are read from the TypesDB (see <a href='http://collectd.org/documentation/manpages/collectd-snmp.5.shtml'>collectd.conf(5)</a>)."))
 snmp_data_type.default = "if_octets"
 
 -- collectd_snmp_data.table
-snmp_data_table = snmp:option(Flag, "Table", translate("Tables Enabled"))
-snmp_data_table.enabled="true"
-snmp_data_table.disabled="false"
+snmp_data_table = snmp:option(ListValue, 'Table', translate('Table'), translate('Define if this is a single list of values or a table of values.'))
+snmp_data_table:value("true", translate("True"))
+snmp_data_table:value("false", translate("False"))
+snmp_data_table.default = "True"
 
 -- collectd_snmp_data.instance
 -- Instance "IF-MIB::ifDescr"
-snmp_data_instance = snmp:option(Value, "Instance", translate("Instance"))
+snmp_data_instance = snmp:option(Value, 'Instance', translate('Instance'), translate('If Table is set to true, Instance is interpreted as an SNMP-prefix that will return a list of values.'))
 snmp_data_instance.default = "IF-MIB::ifDescr"
 
 -- collectd_snmp_data.values
 -- Values "IF-MIB::ifInOctets" "IF-MIB::ifOutOctets"
-snmp_data_values = snmp:option(Value, "Values", translate("Values"))
+snmp_data_values = snmp:option(Value, 'Values', translate('Values OID'), translate('If Table is set to true, each OID must be the prefix of all the values to query.'))
 snmp_data_values.default = '"IF-MIB::ifInOctets" "IF-MIB::ifOutOctets"'
 
--- </Data>
+snmp_data_scale = snmp:option(Value, 'Scale', translate('Scale Value'), translate('The gauge-values returned by the SNMP-agent are multiplied by Scale Value.'))
+snmp_data_scale.rmempty = true
+snmp_data_scale.optional = true
+
+snmp_data_shift = snmp:option(Value, 'Shift', translate('Shift Value'), translate('Shift Value is added to gauge-values returned by the SNMP-agent after they have been multiplied by any Scale value.'))
+snmp_data_shift.rmempty = true
+snmp_data_shift.optional = true
 
 -- <Host "some.switch.mydomain.org">
 -- collectd_snmp_hosts config section
-
 host = m:section(TypedSection, "collectd_snmp_host",
 	translate("SNMP Host Section"),
-	translate("Here you define the hosts you wish to collect data from" ))
+	translate("The Host block defines which hosts to query, which SNMP community and version to use and which of the defined Data to query. The argument passed to the Host block is used as the hostname in the data stored by collectd." ))
 	host.addremove = true
 	host.anonymous = true
 
@@ -76,26 +82,24 @@ snmp_host_address.default = "192.168.0.2"
 
 -- collectd_host_snmpVersion
 -- Version 1
-snmp_host_snmpVersion = host:option(ListValue, "Version", translate("Version"))
+snmp_host_snmpVersion = host:option(ListValue, "Version", translate("SNMP Version"), translate("Set the SNMP version to use."))
 snmp_host_snmpVersion:value("1", translate("v1"))
-snmp_host_snmpVersion:value("2", translate("v2"))
-snmp_host_snmpVersion:value("3", translate("v3"))
+snmp_host_snmpVersion:value("2", translate("v2c"))
+snmp_host_snmpVersion.default = "2"
 
 -- collectd_host_communityString
 -- Community "community_string"
-snmp_host_communityString = host:option(Value, "Community", translate("Community"))
+snmp_host_communityString = host:option(Value, "Community", translate("Community String"), translate("Pass <i>Community</i> to the host."))
 snmp_host_communityString.default = "community_string"
 
 -- collectd_host_trafficName
 -- Collect "std_traffic"
-snmp_host_trafficName = host:option(Value, "Collect", translate("Collect"))
+snmp_host_trafficName = host:option(Value, "Collect", translate("Collect Data Block"), translate("Defines which values to collect. Data refers to one of the Data block above. Since the config file is read top-down you need to define the data before using it here."))
 snmp_host_trafficName.default = "std_traffic"
 
 -- collectd_host_interval
 -- Interval 120
-snmp__host_interval = host:option(Value, "Interval", translate("Interval"))
+snmp__host_interval = host:option(Value, "Interval", translate("Interval (Seconds)"), translate("Collect data from this host every x seconds. This option is meant for devices with not much CPU power, e. g. network equipment such as switches, embedded devices, rack monitoring systems and so on. Since the Step of generated RRD files depends on this setting it's wise to select a reasonable value once and never change it."))
 snmp__host_interval.default = "120"
-
--- </Host> 
 
 return m
